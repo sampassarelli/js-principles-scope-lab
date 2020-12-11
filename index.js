750b3d764e7199193147e24e63c83f9a175c1092
sampassarelli <passarelli.sam@gmail.com>
2020-12-10 19:48:19 -0600

    commit tree 4b1ce6a4c01caa215a59f66c5baa880851489c6b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Write your solution in this 
+var customerName = "bob"
+const leastFavoriteCustomer = "someone else"
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "sam"
+}
+
